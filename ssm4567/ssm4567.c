34d650b40254a16a9c4eb0f7896f9125ca41250b
CoolStar <coolstarorganization@gmail.com>
2022-02-23 20:14:15 -0800

    Call SST + Fix speakers on Chell

--- ssm4567/ssm4567.c
+++ ssm4567/ssm4567.c
@@ -1,6 +1,7 @@
-#define DESCRIPTOR_DEF
 #include "ssm4567.h"
 #include "registers.h"
+
+#define bool int
 
 static ULONG Ssm4567DebugLevel = 100;
 static ULONG Ssm4567DebugCatagories = DBG_INIT || DBG_PNP || DBG_IOCTL;
@@ -193,6 +194,290 @@ Exit:
 	return status;
 }
 
+int IntCSSTArg2 = 1;
+
+VOID
+UpdateIntcSSTStatus(
+	IN PSSM4567_CONTEXT pDevice,
+	int sstStatus
+) {
+	IntcSSTArg* SSTArg = &pDevice->sstArgTemp;
+	RtlZeroMemory(SSTArg, sizeof(IntcSSTArg));
+
+	if (pDevice->IntcSSTHwMultiCodecCallback) {
+		if (sstStatus != 1 || pDevice->IntcSSTStatus) {
+			SSTArg->chipModel = 4567;
+			SSTArg->caller = 0xc0000165; //gmaxcodec
+			if (sstStatus) {
+				if (sstStatus == 1) {
+					if (!pDevice->IntcSSTStatus) {
+						return;
+					}
+					SSTArg->sstQuery = 12;
+					SSTArg->dword11 = 2;
+					SSTArg->querySize = 21;
+				}
+				else {
+					SSTArg->sstQuery = 11;
+					SSTArg->querySize = 20;
+				}
+
+				SSTArg->deviceInD0 = (pDevice->DevicePoweredOn != 0);
+			}
+			else {
+				SSTArg->sstQuery = 10;
+				SSTArg->querySize = 18;
+				SSTArg->deviceInD0 = 1;
+			}
+			ExNotifyCallback(pDevice->IntcSSTHwMultiCodecCallback, SSTArg, &IntCSSTArg2);
+		}
+	}
+}
+
+VOID
+IntcSSTWorkItemFunc(
+	IN WDFWORKITEM  WorkItem
+)
+{
+	WDFDEVICE Device = (WDFDEVICE)WdfWorkItemGetParentObject(WorkItem);
+	PSSM4567_CONTEXT pDevice = GetDeviceContext(Device);
+
+	UpdateIntcSSTStatus(pDevice, 0);
+}
+
+DEFINE_GUID(GUID_SST_RTK_1,
+	0xDFF21CE2, 0xF70F, 0x11D0, 0xB9, 0x17, 0x00, 0xA0, 0xC9, 0x22, 0x31, 0x96); //Headphones
+DEFINE_GUID(GUID_SST_RTK_2,
+	0xDFF21CE1, 0xF70F, 0x11D0, 0xB9, 0x17, 0x00, 0xA0, 0xC9, 0x22, 0x31, 0x96); //InsideMobileLid
+DEFINE_GUID(GUID_SST_RTK_3,
+	0xDFF21BE1, 0xF70F, 0x11D0, 0xB9, 0x17, 0x00, 0xA0, 0xC9, 0x22, 0x31, 0x96); //Also InsideMobileLid?
+DEFINE_GUID(GUID_SST_RTK_4,
+	0xDFF21FE3, 0xF70F, 0x11D0, 0xB9, 0x17, 0x00, 0xA0, 0xC9, 0x22, 0x31, 0x96); //Line out
+
+VOID
+IntcSSTCallbackFunction(
+	IN WDFWORKITEM  WorkItem,
+	IntcSSTArg* SSTArgs,
+	PVOID Argument2
+) {
+	if (!WorkItem) {
+		return;
+	}
+	WDFDEVICE Device = (WDFDEVICE)WdfWorkItemGetParentObject(WorkItem);
+	PSSM4567_CONTEXT pDevice = GetDeviceContext(Device);
+
+	if (Argument2 == &IntCSSTArg2) {
+		return;
+	}
+
+	//gmaxCodec checks that querySize is greater than 0x10 first thing
+	if (SSTArgs->querySize <= 0x10) {
+		return;
+	}
+
+	//Intel Caller: 0xc00000a3 (STATUS_DEVICE_NOT_READY)
+	//GMax Caller: 0xc0000165
+
+	if (SSTArgs->chipModel == 4567) {
+		/*
+
+		Gmax (no SST driver):
+			init:	sstQuery = 10
+					dwordc = 18
+					deviceInD0 = 1
+
+			stop:	sstQuery = 11
+					dwordc = 20
+					deviceInD0 = 0
+		*/
+
+		/*
+
+		Gmax (SST driver)
+			post-init:	sstQuery = 12
+						dwordc = 21
+						dword11 = 2
+
+		*/
+		if (Argument2 != &IntCSSTArg2) { //Intel SST is calling us
+			bool checkCaller = (SSTArgs->caller != 0);
+
+			if (SSTArgs->sstQuery == 11) {
+				if (SSTArgs->querySize >= 0x15) {
+					if (SSTArgs->deviceInD0 == 0) {
+						pDevice->IntcSSTStatus = 0; //SST is inactive
+						SSTArgs->caller = STATUS_SUCCESS;
+						//mark device as inactive?
+					}
+					else {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER_5;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//SST Query 1:
+			//	sstQuery: 10, querySize: 0x9e, dword11: 0x0
+			//	deviceInD0: 0x1, byte25: 0
+
+			if (SSTArgs->sstQuery == 10) { //gmax responds no matter what
+				if (SSTArgs->querySize >= 0x15) {
+					if (SSTArgs->deviceInD0 == 1) {
+						pDevice->IntcSSTStatus = 1;
+						SSTArgs->caller = STATUS_SUCCESS;
+						//mark device as active??
+					}
+					else {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER_5;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//SST Query 2:
+			//	sstQuery: 2048, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 0, byte25: 0
+
+			if (SSTArgs->sstQuery == 2048) {
+				if (SSTArgs->querySize >= 0x11) {
+					SSTArgs->deviceInD0 = 1;
+					SSTArgs->caller = STATUS_SUCCESS;
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//SST Query 3:
+			//	sstQuery: 2051, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 0, byte25: 0
+
+			if (SSTArgs->sstQuery == 2051) {
+				if (SSTArgs->querySize >= 0x9E) {
+					if (SSTArgs->deviceInD0) {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER;
+					}
+					else {
+
+						SSTArgs->deviceInD0 = 0;
+						SSTArgs->dword11 = (1 << 24) | 0;
+
+						SSTArgs->guid = GUID_SST_RTK_2;
+
+						SSTArgs->byte25 = 1;
+						SSTArgs->dword26 = KSAUDIO_SPEAKER_STEREO; //Channel Mapping
+						SSTArgs->dword2A = JACKDESC_RGB(255, 174, 201); //Color (gmax sets to 0)
+						SSTArgs->dword2E = eConnTypeOtherAnalog; //EPcxConnectionType
+						SSTArgs->dword32 = eGeoLocInsideMobileLid; //EPcxGeoLocation
+						SSTArgs->dword36 = eGenLocInternal; //genLocation?
+						SSTArgs->dword3A = ePortConnIntegratedDevice; //portConnection?
+						SSTArgs->dword3E = 1; //isConnected?
+						SSTArgs->byte42 = 0;
+						SSTArgs->byte43 = 0;
+						SSTArgs->caller = STATUS_SUCCESS;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//This is the minimum for SST to initialize. Everything after is extra
+			//SST Query 4:
+			//	sstQuery: 2054, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 0, byte25: 0
+			if (SSTArgs->sstQuery == 2054) {
+				if (SSTArgs->querySize >= 0x9E) {
+					if (SSTArgs->deviceInD0) {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER;
+					}
+					else {
+						SSTArgs->dword11 = 2;
+						SSTArgs->caller = STATUS_SUCCESS;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//SST Query 5:
+			//	sstQuery: 2055, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 0, byte25: 0
+
+			if (SSTArgs->sstQuery == 2055) {
+				if (SSTArgs->querySize < 0x22) {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+				else {
+					SSTArgs->caller = STATUS_NOT_SUPPORTED;
+				}
+			}
+
+			//SST Query 6:
+			//	sstQuery: 13, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 1, byte25: 0
+			if (SSTArgs->sstQuery == 13) {
+				if (SSTArgs->querySize >= 0x14) {
+					if (SSTArgs->deviceInD0) {
+						pDevice->IntcSSTStatus = 1;
+						SSTArgs->caller = STATUS_SUCCESS;
+
+						//UpdateIntcSSTStatus(pDevice, 1);
+					}
+					else {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			//SST Query 7:
+			//	sstQuery: 2064, querySize: 0x9e, dword11: 0x00
+			//	deviceInD0: 0, byte25: 0
+			if (SSTArgs->sstQuery == 2064) {
+				if (SSTArgs->querySize >= 0x19) {
+					if (!SSTArgs->deviceInD0) {
+						unsigned int data1 = SSTArgs->guid.Data1;
+						//DbgPrint("data1: %d\n", data1);
+						if (data1 != -1 && data1 < 1) {
+							SSTArgs->dword11 = 0; //no feedback on ssm4567
+							SSTArgs->caller = STATUS_SUCCESS;
+						}
+						else {
+							SSTArgs->caller = STATUS_INVALID_PARAMETER;
+						}
+					}
+					else {
+						SSTArgs->caller = STATUS_INVALID_PARAMETER;
+					}
+				}
+				else {
+					SSTArgs->caller = STATUS_BUFFER_TOO_SMALL;
+				}
+			}
+
+			if (checkCaller) {
+				if (SSTArgs->caller != STATUS_SUCCESS) {
+					//DbgPrint("Warning: Returned error 0x%x; query: %d\n", SSTArgs->caller, SSTArgs->sstQuery);
+				}
+			}
+		}
+	}
+	else {
+		//On SST Init: chipModel = 0, caller = 0xc00000a3, sstQuery = 10, dwordc: 0x9e
+
+		if (SSTArgs->sstQuery == 10 && pDevice->IntcSSTWorkItem) {
+			WdfWorkItemEnqueue(pDevice->IntcSSTWorkItem); //SST driver was installed after us...
+		}
+	}
+}
 
 NTSTATUS
 OnPrepareHardware(
@@ -292,7 +577,24 @@ Status
 	if (!NT_SUCCESS(status)) {
 		return status;
 	}
-	DbgPrint("UID: %d\n", pDevice->UID);
+	pDevice->SetUID = TRUE;
+
+	if (pDevice->UID == 0) {
+		WDF_OBJECT_ATTRIBUTES attributes;
+		WDF_WORKITEM_CONFIG workitemConfig;
+
+		WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+		WDF_OBJECT_ATTRIBUTES_SET_CONTEXT_TYPE(&attributes, SSM4567_CONTEXT);
+		attributes.ParentObject = FxDevice;
+		WDF_WORKITEM_CONFIG_INIT(&workitemConfig, IntcSSTWorkItemFunc);
+		status = WdfWorkItemCreate(&workitemConfig,
+			&attributes,
+			&pDevice->IntcSSTWorkItem);
+		if (!NT_SUCCESS(status))
+		{
+			return status;
+		}
+	}
 
 	return status;
 }
@@ -323,7 +625,73 @@ Status
 
 	UNREFERENCED_PARAMETER(FxResourcesTranslated);
 
+	if (pDevice->SetUID && pDevice->UID == 0) {
+		UpdateIntcSSTStatus(pDevice, 2);
+	}
+
 	SpbTargetDeinitialize(FxDevice, &pDevice->I2CContext);
+
+	if (pDevice->IntcSSTCallbackObj) {
+		ExUnregisterCallback(pDevice->IntcSSTCallbackObj);
+		pDevice->IntcSSTCallbackObj = NULL;
+	}
+
+	if (pDevice->IntcSSTWorkItem) {
+		WdfWorkItemFlush(pDevice->IntcSSTWorkItem);
+		WdfObjectDelete(pDevice->IntcSSTWorkItem);
+		pDevice->IntcSSTWorkItem = NULL;
+	}
+
+	if (pDevice->IntcSSTHwMultiCodecCallback) {
+		ObfDereferenceObject(pDevice->IntcSSTHwMultiCodecCallback);
+		pDevice->IntcSSTHwMultiCodecCallback = NULL;
+	}
+
+	return status;
+}
+
+NTSTATUS
+OnSelfManagedIoInit(
+	_In_
+	WDFDEVICE FxDevice
+) {
+	PSSM4567_CONTEXT pDevice = GetDeviceContext(FxDevice);
+	NTSTATUS status = STATUS_SUCCESS;
+
+	if (!pDevice->SetUID) {
+		status = STATUS_INVALID_DEVICE_STATE;
+		return status;
+	}
+
+	if (pDevice->UID == 0) { //Hook onto the first SSM codec
+		UNICODE_STRING IntcAudioSSTMultiHwCodecAPI;
+		RtlInitUnicodeString(&IntcAudioSSTMultiHwCodecAPI, L"\\CallBack\\IntcAudioSSTMultiHwCodecAPI");
+
+
+		OBJECT_ATTRIBUTES attributes;
+		InitializeObjectAttributes(&attributes,
+			&IntcAudioSSTMultiHwCodecAPI,
+			OBJ_KERNEL_HANDLE | OBJ_OPENIF | OBJ_CASE_INSENSITIVE | OBJ_PERMANENT,
+			NULL,
+			NULL
+		);
+		status = ExCreateCallback(&pDevice->IntcSSTHwMultiCodecCallback, &attributes, TRUE, TRUE);
+		if (!NT_SUCCESS(status)) {
+
+			return status;
+		}
+
+		pDevice->IntcSSTCallbackObj = ExRegisterCallback(pDevice->IntcSSTHwMultiCodecCallback,
+			IntcSSTCallbackFunction,
+			pDevice->IntcSSTWorkItem
+		);
+		if (!pDevice->IntcSSTCallbackObj) {
+
+			return STATUS_NO_CALLBACK_ACTIVE;
+		}
+
+		UpdateIntcSSTStatus(pDevice, 0);
+	}
 
 	return status;
 }
@@ -355,6 +723,11 @@ Status
 	PSSM4567_CONTEXT pDevice = GetDeviceContext(FxDevice);
 	NTSTATUS status = STATUS_SUCCESS;
 
+	if (!pDevice->SetUID) {
+		status = STATUS_INVALID_DEVICE_STATE;
+		return status;
+	}
+
 	//Power on Amp
 	status = ssm4567_set_power(pDevice, TRUE);
 	if (!NT_SUCCESS(status)) {
@@ -362,13 +735,15 @@ Status
 	}
 
 	//Set Format
+	uint8_t fmt = SSM4567_SAI_CTRL_1_BCLK | SSM4567_SAI_CTRL_1_TDM;
+
 	status = ssm4567_reg_update(pDevice, SSM4567_REG_SAI_CTRL_1,
 		SSM4567_SAI_CTRL_1_BCLK |
 		SSM4567_SAI_CTRL_1_FSYNC |
 		SSM4567_SAI_CTRL_1_LJ |
 		SSM4567_SAI_CTRL_1_TDM |
 		SSM4567_SAI_CTRL_1_PDM,
-		0);
+		fmt);
 	if (!NT_SUCCESS(status)) {
 		return status;
 	}
@@ -393,8 +768,7 @@ Status
 	}
 
 	//Set TDM Slot
-	int slot = pDevice->UID + 1;
-	DbgPrint("Set Slot %d\n", slot);
+	int slot = pDevice->UID;
 	status = ssm4567_reg_update(pDevice, SSM4567_REG_SAI_CTRL_2, SSM4567_SAI_CTRL_2_AUTO_SLOT | SSM4567_SAI_CTRL_2_TDM_SLOT_MASK, SSM4567_SAI_CTRL_2_TDM_SLOT(slot));
 	if (!NT_SUCCESS(status)) {
 		return status;
@@ -424,11 +798,32 @@ Status
 		return status;
 	}
 
-	//Set Default Volume
-	status = ssm4567_reg_update(pDevice, SSM4567_REG_DAC_VOLUME, 0xFF, 0x40); //+0 db
+	//Disable Battery/Voltage/Current Sense
+	status = ssm4567_reg_update(pDevice, SSM4567_REG_POWER_CTRL,
+		SSM4567_POWER_BSNS_PWDN |
+		SSM4567_POWER_VSNS_PWDN |
+		SSM4567_POWER_ISNS_PWDN,
+		SSM4567_POWER_BSNS_PWDN |
+		SSM4567_POWER_VSNS_PWDN |
+		SSM4567_POWER_ISNS_PWDN);
 	if (!NT_SUCCESS(status)) {
 		return status;
 	}
+
+	//Set Default Volume
+	status = ssm4567_reg_update(pDevice, SSM4567_REG_DAC_VOLUME, 0xFF, 0x40); //0x40 = 0 db
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+
+	/*for (int i = 0; i <= 0x16; i++) {
+		unsigned int data;
+		if (NT_SUCCESS(ssm4567_reg_read(pDevice, i, &data))) {
+			DbgPrint("Reg 0x%x: 0x%x\n", i, data);
+		}
+	}*/
+
+	pDevice->DevicePoweredOn = TRUE;
 
 	return status;
 }
@@ -465,6 +860,8 @@ Status
 		return status;
 	}
 
+	pDevice->DevicePoweredOn = FALSE;
+
 	return STATUS_SUCCESS;
 }
 
@@ -490,20 +887,13 @@ Ssm4567EvtDeviceAdd(
 	Ssm4567Print(DEBUG_LEVEL_INFO, DBG_PNP,
 		"Ssm4567EvtDeviceAdd called\n");
 
-	//
-	// Tell framework this is a filter driver. Filter drivers by default are  
-	// not power policy owners. This works well for this driver because
-	// HIDclass driver is the power policy owner for HID minidrivers.
-	//
-
-	WdfFdoInitSetFilter(DeviceInit);
-
 	{
 		WDF_PNPPOWER_EVENT_CALLBACKS pnpCallbacks;
 		WDF_PNPPOWER_EVENT_CALLBACKS_INIT(&pnpCallbacks);
 
 		pnpCallbacks.EvtDevicePrepareHardware = OnPrepareHardware;
 		pnpCallbacks.EvtDeviceReleaseHardware = OnReleaseHardware;
+		pnpCallbacks.EvtDeviceSelfManagedIoInit = OnSelfManagedIoInit;
 		pnpCallbacks.EvtDeviceD0Entry = OnD0Entry;
 		pnpCallbacks.EvtDeviceD0Exit = OnD0Exit;
 
--- ssm4567/ssm4567.h
+++ ssm4567/ssm4567.h
@@ -12,10 +12,17 @@
 #pragma warning(default:4214)
 #include <wdf.h>
 
+#include <portcls.h>
+
 #include <acpiioct.h>
 #include <ntstrsafe.h>
 
+#include <stdint.h>
+
 #include "spb.h"
+
+#define JACKDESC_RGB(r, g, b) \
+    ((COLORREF)((r << 16) | (g << 8) | (b)))
 
 //
 // String definitions
@@ -24,6 +31,46 @@
 #define DRIVERNAME                 "ssm4567.sys: "
 
 #define SSM4567_POOL_TAG            (ULONG) 'B343'
+
+#define true 1
+#define false 0
+
+#pragma pack(push,1)
+typedef struct _IntcSSTArg
+{
+	int32_t chipModel;
+	int32_t sstQuery;
+	int32_t caller;
+	int32_t querySize;
+
+#ifdef __GNUC__
+	char EndOfHeader[0];
+#endif
+
+	uint8_t deviceInD0;
+#ifdef __GNUC__
+	char EndOfPowerCfg[0];
+#endif
+
+	int32_t dword11;
+	GUID guid;
+
+#ifdef __GNUC__
+	char EndOfGUID[0];
+#endif
+	uint8_t byte25;
+	int32_t dword26;
+	int32_t dword2A;
+	int32_t dword2E;
+	int32_t dword32;
+	int32_t dword36;
+	int32_t dword3A;
+	int32_t dword3E;
+	uint8_t byte42;
+	uint8_t byte43;
+	char padding[90]; //idk what this is for
+}  IntcSSTArg, * PIntcSSTArg;
+#pragma pack(pop)
 
 typedef struct _SSM4567_CONTEXT
 {
@@ -34,7 +81,17 @@ typedef struct _SSM4567_CONTEXT
 
 	SPB_CONTEXT I2CContext;
 
+	BOOLEAN SetUID;
 	INT32 UID;
+
+	BOOLEAN DevicePoweredOn;
+	INT8 IntcSSTStatus;
+
+	WDFWORKITEM IntcSSTWorkItem;
+	PCALLBACK_OBJECT IntcSSTHwMultiCodecCallback;
+	PVOID IntcSSTCallbackObj;
+
+	IntcSSTArg sstArgTemp;
 
 } SSM4567_CONTEXT, *PSSM4567_CONTEXT;
 
@@ -66,7 +123,7 @@ EVT_WDF_IO_QUEUE_IO_INTERNAL_DEVICE_CONTROL Ssm4567EvtInternalDeviceControl;
 #define DBG_PNP   2
 #define DBG_IOCTL 4
 
-#if 1
+#if 0
 #define Ssm4567Print(dbglevel, dbgcatagory, fmt, ...) {          \
     if (Ssm4567DebugLevel >= dbglevel &&                         \
         (Ssm4567DebugCatagories && dbgcatagory))                 \
