97f2b38b999e0de2c2ebba2587029dcb27ca588d
CoolStar <coolstarorganization@gmail.com>
2022-12-15 01:15:28 -0800

    Allow disabling in device manager

--- ssm4567/ssm4567.c
+++ ssm4567/ssm4567.c
@@ -1027,6 +1027,14 @@ Ssm4567EvtDeviceAdd(
 		return status;
 	}
 
+	{
+		WDF_DEVICE_STATE deviceState;
+		WDF_DEVICE_STATE_INIT(&deviceState);
+
+		deviceState.NotDisableable = WdfFalse;
+		WdfDeviceSetDeviceState(device, &deviceState);
+	}
+
 	WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig, WdfIoQueueDispatchParallel);
 
 	queueConfig.EvtIoInternalDeviceControl = Ssm4567EvtInternalDeviceControl;
